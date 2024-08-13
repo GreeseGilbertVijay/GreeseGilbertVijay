4ef72e7fcef9c52473d9b2c5137718d7d4397a94
greesevijaygilbert18@gmail.com <greesevijaygilbert18@gmail.com>
2024-08-13 20:59:15 +0530

    content changes

--- components/columns.jsx
+++ components/columns.jsx
@@ -13,7 +13,7 @@ export const Columns = () => {
         <CgWebsite className='column-icons'/>
         <h3>Static Websites</h3>
         <p>The website designed to represent a company or organization,
-           providing information about products, services,locations
+           providing information about products, services, locations
             and contact details.</p>
         </div>
 
@@ -21,7 +21,7 @@ export const Columns = () => {
         <CiShop className='column-icons' />
         <h3>E-Commerce</h3>
         <p>The website stores where products or services
-           are sold directly to consumers.Detailed descriptions, images, prices,
+           are sold directly to consumers. Detailed descriptions, images, prices,
             and specifications of products or services.
         </p>
         </div>
@@ -29,7 +29,7 @@ export const Columns = () => {
         <div className="columns-box">
         <RiReactjsFill className='column-icons'/>
         <h3>React Js</h3>
-        <p>Work with React.js emphasizes a clean and maintainable codebase,components,styles,
+        <p>Work with React.js emphasizes a clean and maintainable codebase, components, styles,
           resulting in highly performant and user-friendly applications.
         </p>
         </div>
@@ -38,7 +38,8 @@ export const Columns = () => {
         <RiNextjsLine  className='column-icons'/>
         <h3>Next Js</h3>
         <p>Built dynamic, responsive applications with a focus on performance and user experience. 
-          By utilizing Next.js, they ensure server-side rendering and optimized static generation, resulting in fast and efficient websites
+          By utilizing Next.js and ensure server-side rendering and optimized static generation, 
+          resulting in fast and efficient websites
         </p>
         </div>
 
